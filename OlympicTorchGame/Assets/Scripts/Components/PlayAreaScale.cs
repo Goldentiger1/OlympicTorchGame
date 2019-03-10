8cd89aecb135b54dcafd8416dc7c33f8fe306710
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2019-03-10 23:29:12 +0200

    Hieman hommia viikonloppuna

---
+++ OlympicTorchGame/Assets/Scripts/Components/PlayAreaScale.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAreaScale : MonoBehaviour 
+{
+    private GameObject PlayAreaTop;
+    private GameObject PlayAreaBottom;
+    private GameObject PlayAreaPointA;
+    private GameObject PlayAreaPointB;
+    public Transform PlayAreaHeight;
+    public Transform PlayAreaWidth;
+
+    private void Awake()
+    {
+        PlayAreaTop = GameObject.Find("PlayAreaTop");
+        PlayAreaBottom = GameObject.Find("PlayAreaBottom");
+
+    }
+}
