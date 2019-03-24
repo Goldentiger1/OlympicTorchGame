0604a27584f9439a63c053e77821dd99e6c30afc
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2019-03-24 23:40:22 +0200

    Hieman hommaa kotona

--- OlympicTorchGame/Assets/Scripts/Components/TorchIgnite.cs
+++ OlympicTorchGame/Assets/Scripts/Components/TorchIgnite.cs
@@ -7,6 +7,7 @@ public class TorchIgnite : MonoBehaviour
     public GameObject torch;
     public GameObject lighter;
     public Color newColor;
+    public Renderer rend;
 
     private void Start()
     {
@@ -14,9 +15,16 @@ public class TorchIgnite : MonoBehaviour
         lighter = GameObject.Find("Lighting Point");
     }
 
+    private void Update()
+    {
+        newColor = Color.green;
+        rend = lighter.GetComponentInChildren<Renderer>();
+    }
+
     public void LightingOlympicFire()
     {
-
+        rend.material.shader 
+        
     }
 
 }
