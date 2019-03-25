125d90adf6c35042a8d528536513e8ebb9e15fff
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2019-03-25 15:51:34 +0200

    Koodia

---
+++ OlympicTorchGame/Assets/Scripts/Components/OnTriggerEnter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerEnter : MonoBehaviour
+{
+    TorchIgnite flame;
+
+    private void Start()
+    {
+        GameObject g = GameObject.Find("Olympic Torch");
+        flame = g.GetComponent<TorchIgnite>();
+    }
+    /*
+     void OnTriggerEnter(Collider other)
+    {
+        flame.LightingOlympicFire();
+    }
+    */
+}
--- OlympicTorchGame/Assets/Scripts/Components/TorchIgnite.cs
+++ OlympicTorchGame/Assets/Scripts/Components/TorchIgnite.cs
@@ -6,8 +6,6 @@ public class TorchIgnite : MonoBehaviour
 {
     public GameObject torch;
     public GameObject lighter;
-    public Color newColor;
-    public Renderer rend;
 
     private void Start()
     {
@@ -15,16 +13,9 @@ public class TorchIgnite : MonoBehaviour
         lighter = GameObject.Find("Lighting Point");
     }
 
-    private void Update()
-    {
-        newColor = Color.green;
-        rend = lighter.GetComponentInChildren<Renderer>();
-    }
-
     public void LightingOlympicFire()
     {
-       // rend.material.shader 
-        
+        lighter.gameObject.GetComponentInChildren<Renderer>().material.color = Color.green;
     }
 
 }
