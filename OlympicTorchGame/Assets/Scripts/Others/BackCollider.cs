45adcd4c6df4d0399afc3c30c1ee9a777113cd2c
Mathias Mustonen <mathias.mustonen@gmail.com>
2019-06-10 03:03:14 +0300

    Foo

---
+++ OlympicTorchGame/Assets/Scripts/Others/BackCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BackCollider : MonoBehaviour
+{
+    private void Update()
+    {
+        MovePosition(PlayerEngine.Instance.headCollider.transform.position);
+    }
+
+    private void MovePosition(Vector3 position)
+    {
+        transform.position = new Vector3(position.x, transform.position.y, position.z);
+
+     
+    }
+}
